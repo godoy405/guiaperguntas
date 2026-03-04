a8a76dd391e4fa33657902b94086cf38d8b83839
Gilberto Neri Godoy <gilbertonerigodoy@gmail.com>
2026-03-03 22:45:32 -0300

    Finalização do projeto

--- database/Pergunta.js
+++ database/Pergunta.js
@@ -13,7 +13,7 @@ const Pergunta = connection.define('perguntas', {
 });
 
 Pergunta.sync({force: false}).then(() => {
-    console.log('Tabela Pergunta criada com sucesso!');
+    console.log('Todos os modelos sincronizados');
 }).catch(err => {
     console.error('Erro ao criar a tabela Pergunta:', err);
 });
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
 const Pergunta = require('./database/Pergunta');
+const Resposta = require('./database/Resposta');
 
 // Testando a conexão com o banco de dados
 connection.authenticate()
@@ -55,17 +56,34 @@ app.get('/pergunta/:id', (req, res) => {
     }
   }).then(pergunta => {
     if(pergunta != undefined) {
-      res.render('pergunta',{
-        pergunta: pergunta
-      }
 
-      );
+       Resposta.findAll({
+        where: { perguntaId: pergunta.id },
+        order: [['id', 'DESC']]
+      }).then(respostas => {
+        res.render('pergunta',{
+        pergunta: pergunta,
+        respostas: respostas
+      });
+    });
+      
     }else{
       res.redirect('/');
     }  
 
   }); 
    
+});
+
+app.post('/responder', (req, res) => {
+  var corpo = req.body.corpo;
+  var perguntaId = req.body.perguntaId;
+  Resposta.create({ 
+    corpo: corpo,
+    perguntaId: perguntaId
+  }).then(() => {
+    res.redirect('/pergunta/' + perguntaId);
+  });
 });
 
 
